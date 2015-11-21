2ffa54c99310bbb6102f18c1bfff1789b7d9f265
Urvesh <urveshpatel94@yahoo.com>
2015-11-21 17:12:37 -0500

    EVEN BETTER UPDATE

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -85,21 +85,64 @@ app.controller('NewProjectModalCtrl', function($scope, $http, $modalInstance) {
     }
 });
 
-app.controller('TaskCtrl', function($scope, $routeParams, $http) {
+app.controller('TaskCtrl', function($scope, $routeParams, $http, $modal) {
     var projectId = $routeParams.projectId;
-    $scope.tasks = [];
 
-    $http({
-        method: 'POST',
-        url: 'php/gettasks.php',
-        data: {
-            projectId: projectId
-        }
-    }).then(function(response) {
-        console.log(response);
-        $scope.tasks = response.data;
-    }, function(err) {
-        console.log(err);
-    });
+    onLoad();
 
+    function onLoad() {
+        $http({
+            method: 'POST',
+            url: 'php/gettasks.php',
+            data: {
+                projectId: projectId
+            }
+        }).then(function(response) {
+            console.log(response);
+            $scope.tasks = response.data;
+        }, function(err) {
+            console.log(err);
+        });
+    }
+
+    $scope.newTask = function(size) {
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'newTaskModal.html',
+            controller: 'NewTaskModalCtrl',
+            size: size
+        });
+
+        modalInstance.result.then(function () {
+            onLoad();
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    }
+});
+
+app.controller('NewTaskModalCtrl', function ($scope, $http, $modalInstance) {
+
+    $scope.ok = function() {
+        if (!$scope.title) return;
+
+        $http({
+            method: 'POST',
+            url: 'php/inserttask.php',
+            data: {
+                title: $scope.title,
+                description: $scope.description
+            }
+        }, function(response) {
+            console.log(response);
+        }, function(err) {
+            console.log(err);
+        });
+
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    }
 });
