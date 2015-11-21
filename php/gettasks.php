c1f25fcc21f88c6db9f55e37a33ab593e7d68fb0
ep85 <ericpalumbo@Erics-MacBook-Pro.local>
2015-11-21 17:05:58 -0500

    BIG update

--- php/gettasks.php
+++ php/gettasks.php
@@ -16,7 +16,7 @@ if(!$conn) {
 	exit;
 }
 
-$result = $conn->query("SELECT p.title, p.description, p.id FROM projects p, project_to_user ptu  where ptu.user_id='$userid' and ptu.project_id=p.id " );
+$result = $conn->query("SELECT p.title, p.description FROM projects p, project_to_user ptu  where ptu.user_id='$userid' and ptu.project_id=p.id " );
 while($row = $result->fetch_array(MYSQLI_ASSOC)) {
     $myArray[]=$row;
 }
