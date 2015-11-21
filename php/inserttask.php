0a21093f754324372f8a17578564f36dcf2d0e20
ep85 <ericpalumbo@Erics-MacBook-Pro.local>
2015-11-21 17:17:11 -0500

    update

---
+++ php/inserttask.php
@@ -0,0 +1,32 @@
+<?php
+
+$server="localhost";
+$user="root";
+$datapassword="password";
+$database="taskmanagement";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+$username=$_SESSION["user"];
+$userid=$_SESSION["userid"];
+
+$postdata= file_get_contents("php://input");
+$request=json_decode($postdata);
+@$title= $request->title;
+@$description=$request->description;
+@$projectId=$request->description;
+
+$conn = new mysqli($server, $user, $datapassword, $database);
+if(!$conn) {
+	echo "error";
+	exit;
+}
+
+$result = $conn->query("INSERT INTO task (project_id, title, description) VALUES ( '$projectId' ,'$title', '$description')" );
+
+
+
+$conn->close();
+
