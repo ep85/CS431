8ee7b4f9bf67f9ede8f2b9c4d7a0695c33cd3ac6
Urvesh <urveshpatel94@yahoo.com>
2015-12-05 14:50:23 -0500

    delete task

---
+++ php/deletetask.php
@@ -0,0 +1,31 @@
+<?php
+
+$server="localhost";
+$user="user1";
+$datapassword="password";
+$database="taskmanagement";
+
+session_start();
+$username=$_SESSION["user"];
+$userid=$_SESSION["userid"];
+
+$postdata= file_get_contents("php://input");
+$request=json_decode($postdata);
+@$taskId=$request->taskId;
+
+$conn = new mysqli($server, $user, $datapassword, $database);
+if(!$conn) {
+    echo "error";
+    exit;
+}
+
+$result = $conn->query("DELETE FROM task where id='$taskId'" );
+$result = $conn->query("DELETE FROM task_to_subtask where task_id = '$taskId'");
+
+$conn->close();
+
+
+
+
+
+?>
